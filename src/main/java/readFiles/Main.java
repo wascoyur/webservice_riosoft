17a59a773cf6410879c38c88ad7ad4c8f3f3a3e9
wascoyur <mytem20007@gmail.com>
2019-10-22 22:44:44 +0500

    доступ к содержимому файла ексель получен.

--- src/main/java/readFiles/Main.java
+++ src/main/java/readFiles/Main.java
@@ -1,10 +1,19 @@
 package readFiles;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        HSSFWorkBook hssfWorkBook = new HSSFWorkBook(new FileInputStream("task/092018B1.xlsx"));
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("D:\\Develop\\Java\\Employee_RioSoft\\src\\main\\resources\\task\\092018B1.xlsx");
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        String s = wb.getSheetName(0);
+        System.out.println(s);
     }
 }
