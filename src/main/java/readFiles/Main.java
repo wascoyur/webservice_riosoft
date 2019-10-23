46e9fb5b8eef1bdac7e950bd3b037f5cc64540a7
wascoyur <mytem20007@gmail.com>
2019-10-23 20:47:40 +0500

    заново создана Н2 в resources\db

--- src/main/java/readFiles/Main.java
+++ src/main/java/readFiles/Main.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream("D:\\Develop\\Java\\Employee_RioSoft\\src\\main\\resources\\task\\092018B1.xlsx");
+        FileInputStream fis = new FileInputStream("./src/main/resources/task/092018B1.xlsx");
         XSSFWorkbook wb = new XSSFWorkbook(fis);
         String s = wb.getSheetName(0);
         System.out.println(s);
