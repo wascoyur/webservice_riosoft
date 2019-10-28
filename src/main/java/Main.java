292eb640021f6ebaf04a5483afef0fb2e6bc29a6
wascoyur <mytem20007@gmail.com>
2019-10-28 21:54:05 +0500

    из Майна вынесено чтение файла в РеадДата Объект, содержащий данные из екселя получен

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,20 @@
-package readFiles;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import readFiles.ReadData;
 
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main {
+//    private String path = "./src/main/resources/task/092018B1.xlsx";
     public static void main(String[] args) throws IOException {
-        FileInputStream fis = new FileInputStream("./src/main/resources/task/092018B1.xlsx");
-        XSSFWorkbook wb = new XSSFWorkbook(fis);
-        String s = wb.getSheetName(0);
-        Sheet sheet = wb.getSheet(s);
-        int countRow = sheet.getPhysicalNumberOfRows();
-        int countColumn = sheet.getRow(0).getLastCellNum();
-        for (int i = 0; i <= sheet.getLastRowNum() ; i++) {
 
-        }
-
-        System.out.println(s);
+        ReadData readData = new ReadData(ReadData.getPathToSource());
+//        for (Row row : readData.getSheet()) {
+//            for (Cell cell : row) {
+//                System.out.print(cell.toString());
+//            }
+//            System.out.println();
+//        }
+        System.out.println();
     }
 }
--- src/main/java/readFiles/ReadData.java
+++ src/main/java/readFiles/ReadData.java
@@ -1,4 +1,45 @@
 package readFiles;
 
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class ReadData {
+    private HashMap<Integer, Integer> wbToMass;
+    private static String pathToSource = "./src/main/resources/task/092018B1.xlsx";
+    private XSSFWorkbook wb;
+    private int countRow;
+    private int countSheets;
+    private String nameSheet;
+
+    public ReadData(String pathToSource) throws IOException {
+        this.pathToSource = pathToSource;
+        this.wb  = createWb();
+        this.countSheets = wb.getNumberOfSheets();
+        this.countRow = wb.getSheetIndex("");
+        this.nameSheet = wb.getSheetName(0);
+    }
+
+    private XSSFWorkbook createWb() throws IOException {
+        FileInputStream fis = new FileInputStream(pathToSource);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+        return wb;
+    }
+
+    public Sheet getSheet() {
+        String s = wb.getSheetName(0);
+        Sheet sheet = wb.getSheet(s);
+        return sheet;
+    }
+
+    public XSSFWorkbook getWb() {
+        return wb;
+    }
+
+    public static String getPathToSource() {
+        return pathToSource;
+    }
 }
