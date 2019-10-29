92b68df6c235511269ed52dfe4ecc0c85f4a0371
wascoyur <mytem20007@gmail.com>
2019-10-29 22:18:37 +0500

    добавлен файл создания ДБ. Не работает.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,27 @@
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
+import fillDB.DbConnect;
 import readFiles.ReadData;
-
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
 //    private String path = "./src/main/resources/task/092018B1.xlsx";
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
 
-        ReadData readData = new ReadData(ReadData.getPathToSource());
+//        ReadData readData = new ReadData(ReadData.getPathToSource());
 //        for (Row row : readData.getSheet()) {
 //            for (Cell cell : row) {
 //                System.out.print(cell.toString());
 //            }
 //            System.out.println();
 //        }
+
+        DbConnect connect = new DbConnect();
+        try {
+            connect.setConnection();
+            connect.createTables();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println();
     }
 }
--- src/main/java/fillDB/DbConnect.java
+++ src/main/java/fillDB/DbConnect.java
@@ -3,21 +3,57 @@ package fillDB;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbConnect {
     private Connection connection;
+    private Statement statement;
 
-    public void setConnection() throws SQLException {
+    public void setConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
         try {
-            connection = DriverManager.getConnection("jdbc:h2:file:D:/Develop/Java/Employee_RioSoft/src/main/resources/db", "user", "");
+            this.connection = DriverManager.getConnection("jdbc:h2:mem:inmem");
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        closeConnection();
     }
 
-    private void closeConnection() throws SQLException {
+    public void closeConnection() throws SQLException {
         connection.close();
+    }
+
+    public void createTables() throws SQLException {
+        this.statement = this.connection.createStatement();
+        statement.executeUpdate("CREATE TABLE_NAME 092018B1" +
+                                "(ID  INTEGER not null, primary key" +
+                                "REG_NUM_A              INTEGER not null," +
+                                "NUM_ACC_SEC_B          INTEGER," +
+                                "INP_BAL_C              INTEGER," +
+                                "INP_DRAG_MET_D         INTEGER," +
+                                "INP_BAL_FINISH_E       INTEGER," +
+                                "TURN_REP_DEBIT_F       INTEGER," +
+                                "TURN_REP_DEB_DEPO_G    INTEGER," +
+                                "TURN_REP_KREDIT_H      INTEGER," +
+                                "TURN_REP_I             INTEGER," +
+                                "TURN_REP_DRAG_MET_J    INTEGER," +
+                                "TURN_REP_KRED_FINISH_K INTEGER," +
+                                "OUT_BAL_L              INTEGER," +
+                                "OUT_BAL_DRAG_MET_M     INTEGER," +
+                                "OUT_BAL_FINISH_N       INTEGER );");
+
+        statement.executeUpdate("create table 092018N1" +
+                "(ID    INTEGER not null" +
+                "primary key," +
+                "ORG_REG_ACC INTEGER," +
+                "ORG_NAME    INTEGER);");
+
+
+        statement.executeUpdate(        "create table NAME(" +
+                        "ID            INTEGER not null" +
+                        "primary key," +
+                        "NUM_ACC_PLAN  INTEGER," +
+                        "NAME_ACC_PLAN INTEGER);)");
+        statement.close();
     }
 
 }
